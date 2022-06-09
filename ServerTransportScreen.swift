cb8c7f8f5ac6ecb3d536158e9fa087192c66ee6c
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-06-09 13:46:25 +0300

    Server transport methods added

--- ServerTransportScreen.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ServerTransportScreen.swift
-//  NearbyInteraction-Sandbox
-//
-//  Created by Eduard on 09.06.2022.
-//
-
-import Foundation
---
+++ ServerTransportViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ServerTransportViewController.swift
+//  NearbyInteraction-Sandbox
+//
+//  Created by Eduard on 09.06.2022.
+//
+
+import NearbyInteraction
+import UIKit
+
+final class ServerTransportViewController: UIViewController {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nearbySession.delegate = self
+        uploadDiscoveryToken { [weak self] in
+            self?.fetchDiscoveryTokens()
+        }
+    }
+
+    // MARK: - Private properties
+    private let nearbySession = NISession()
+    private var nearbyObjects: [NINearbyObject] = []
+}
+
+// MARK: - Private methods
+private extension ServerTransportViewController {
+    func uploadDiscoveryToken(completion: @escaping () -> Void) {
+        guard let discoveryToken = nearbySession.discoveryToken,
+              let data = try? NSKeyedArchiver.archivedData(withRootObject: discoveryToken, requiringSecureCoding: true) else { return }
+        APIManager.shared.uploadDiscoveryToken(data, completion: completion)
+    }
+
+    func fetchDiscoveryTokens() {
+        APIManager.shared.fetchDiscoveryTokens { tokens in
+            tokens.filter({
+                $0 != self.nearbySession.discoveryToken
+            }).forEach { token in
+                let config = NINearbyPeerConfiguration(peerToken: token)
+                self.nearbySession.run(config)
+            }
+        }
+    }
+}
+
+// MARK: - NISessionDelegate
+extension ServerTransportViewController: NISessionDelegate {
+    func session(_ session: NISession, didUpdate nearbyObjects: [NINearbyObject]) {
+        print("Nearby objects: \(nearbyObjects.map({ $0.distance }))")
+    }
+}
