77678285ecf00b179bfb763dfea768b7831a8166
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-06-16 07:54:48 +0300

    Device check added

---
+++ NearbyInteraction-Sandbox/Core/Extensions/UIViewController+alert.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController+alert.swift
+//  NearbyInteraction-Sandbox
+//
+//  Created by Eduard on 16.06.2022.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showAlert(title: String, message: String? = nil) {
+        let alert = UIAlertController(title: title, message: message ?? "", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
--- NearbyInteraction-Sandbox/WelcomeScreen/ViewController.swift
+++ NearbyInteraction-Sandbox/WelcomeScreen/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Eduard on 09.06.2022.
 //
 
+import NearbyInteraction
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,15 +17,22 @@ class ViewController: UIViewController {
 
     // MARK: - Actions
     @IBAction func didTapMultipeerButton(_ sender: Any) {
+        guard NISession.isSupported else {
+            showAlert(title: "Your device is not supported")
+            return
+        }
         let storyboard = UIStoryboard(name: "MultipeerViewController", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "MultipeerViewController") as! MultipeerViewController
         navigationController?.pushViewController(vc, animated: true)
     }
 
     @IBAction func didTapServerButton(_ sender: Any) {
+        guard NISession.isSupported else {
+            showAlert(title: "Your device is not supported")
+            return
+        }
         let storyboard = UIStoryboard(name: "ServerViewController", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "ServerViewController") as! ServerViewController
         navigationController?.pushViewController(vc, animated: true)
     }
 }
-
