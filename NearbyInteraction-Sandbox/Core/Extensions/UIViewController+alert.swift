850292aa834b37f6386de0588976be6e6e4e466c
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-08-11 17:32:08 +0300

    Alert moved to main thread

--- NearbyInteraction-Sandbox/Core/Extensions/UIViewController+alert.swift
+++ NearbyInteraction-Sandbox/Core/Extensions/UIViewController+alert.swift
@@ -9,8 +9,10 @@ import UIKit
 
 extension UIViewController {
     func showAlert(title: String, message: String? = nil) {
-        let alert = UIAlertController(title: title, message: message ?? "", preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message ?? "", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 }
